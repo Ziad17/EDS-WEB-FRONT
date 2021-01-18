1e93cba7deb62566bd98ea58ce7af28489c334cc
ZiadFathy <36865821+Ziad17@users.noreply.github.com>
2021-01-18 18:59:38 +0000

    Update test.php

--- test.php
+++ test.php
@@ -1,21 +1,21 @@
 <?php
+// phpinfo();
+
 // PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:dms-kfs.database.windows.net,1433; Database = DMS_db", "ziadmohamd456", "01015790817aA");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    print("sucess");
-    
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
 
 // SQL Server Extension Sample Code:
-//$connectionInfo = array("UID" => "ziadmohamd456", "pwd" => "01015790817aA", "Database" => "DMS_db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-//$serverName = "tcp:dms-kfs.database.windows.net,1433";
-//$conn = sqlsrv_connect($serverName, $connectionInfo);
-//if($conn)
-//{echo 'connection successful';}
-//else{echo 'no connection';}
+$connectionInfo = array("UID" => "ziadmohamd456", "pwd" => "01015790817aA", "Database" => "DMS_db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+ $serverName = "tcp:dms-kfs.database.windows.net,1433";
+$conn =  sqlsrv_connect($serverName, $connectionInfo); 
+
+
+$query="SELECT * FROM Person";
+$stmt = sqlsrv_query($conn, $query);
+
+while($row = sqlsrv_fetch_array($stmt)) {
+    echo $row[1];
+    echo "</br>";
+    
+    //...
+}
 ?>
