e5b3d83ba754a09c7f255b83e48e2d918cd01d3e
Ahmed hesham esmail <47076435+Ahmedheshamesmail@users.noreply.github.com>
2021-02-13 01:23:35 +0200

    Create headtag.php

---
+++ Include/headtag.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  <title>  <?php if (isset($name)) {echo $name;}else{echo "KfsDocs";}  ?></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- logo  -->
+
+  <link rel="icon" type="image/png" href="img/favicon.png" />
+ 	<link rel="stylesheet" href="css/all.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/dropzone.css">
+	<link rel="stylesheet" href="css/style.css">
+  </head>
