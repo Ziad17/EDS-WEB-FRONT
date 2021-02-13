18d24e0c0734bd90e9ef069adf86ff230694ebaa
Ahmed hesham esmail <47076435+Ahmedheshamesmail@users.noreply.github.com>
2021-02-13 03:40:00 +0200

    Create sidebar.php

---
+++ Include/sidebar.php
@@ -0,0 +1,52 @@
+			<nav  id="sidebar">
+				<div class="p-4  sidch">
+		  		<a href="img/undraw_male_avatar_323b.svg"><img style="border-radius: 50%;padding: 5px" src="img/undraw_male_avatar_323b.svg" width="100px" height="100px" alt=""></a>
+					<h6  style="color: #fff;text-shadow: 5px 5px 5px #fff">Eng:Ahmed Hesham</h6>
+					<span class="country">Web Designer</span>
+					<hr style="color: #fff;background-color: #fff;">
+	        <ul  class="list-unstyled components mb-5">
+	          <li class="active">
+	            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-home"></i> Home &nbsp &nbsp </a>
+	            <ul class="collapse list-unstyled" id="homeSubmenu">
+                <li>
+                    <a class="drobli" href="#">Home 1</a>
+                </li>
+                <li>
+                    <a class="drobli" href="#">Home 2</a>
+                </li>
+                <li>
+                    <a class="drobli" href="#">Home 3</a>
+                </li>
+	            </ul>
+	          </li>
+	          <li>
+	              <a href="#"><i class="fas fa-project-diagram"></i> Project</a>
+	          </li>
+	          <li>
+              <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"> <i class="fas fa-university"></i> Colleges </a>
+              <ul class="collapse list-unstyled" id="pageSubmenu">
+                <li>
+                    <a class="drobli" href="#"><i class="fas fa-vials"></i> Science and Engineering</a>
+                </li>
+                <li>
+                    <a class="drobli" href="#"><i class="fas fa-hand-holding-medical"></i> Medical colleges</a>
+                </li>
+                <li>
+                    <a class="drobli" href="#"><i class="far fa-building"></i> Humanitarian and educational</a>
+                </li>
+              </ul>
+	          </li>
+	          <li>
+              <a href="#">Portfolio</a>
+	          </li>
+	          <li>
+              <a  href="#">
+                <button onMouseOver="this.style.color='#f8b739'" onMouseOut="this.style.color='#f8b739'" type="button" class="btn " data-toggle="modal" data-target="#section">
+                  <i class="fas fa-stream"></i>
+                   Create new section
+                </button>
+              </a>
+	          </li>
+	        </ul>
+	      </div>
+    	</nav>
