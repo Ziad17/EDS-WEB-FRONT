896f32dcfbfda4afb847594e3fc42a422df124ee
Ahmed hesham esmail <47076435+Ahmedheshamesmail@users.noreply.github.com>
2021-02-13 03:39:45 +0200

    Create content.php

---
+++ Include/content.php
@@ -0,0 +1,31 @@
+        <div align="center" class="col-md-12">
+            <form action="" method="POST" accept-charset="utf-8">
+              <div class="file-drop-area">
+                <i style="color: orange;font-size: 22px" class="fas fa-cloud-upload-alt"></i>
+                <span class="file-msg">
+                Folder is empty.<br>
+                Click, or drag your first file here!
+                </span>
+                <input class="file-input" type="file" multiple required="">
+              </div>
+              <input class="btn w-25 btn-md btn-warning float-lg-right" type="submit" name="submit" value="uplode">
+            </form>
+              <table class="float-lg-left">
+                <thead>
+                  <tr>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>File Name :</td>
+                  </tr>
+                  <tr>
+                    <td>File Size :</td>
+                  </tr>
+                  <tr>
+                    <td>File Extantion :</td>
+                  </tr>
+                </tbody>
+              </table>
+        </div>
