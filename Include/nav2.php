9262d45350e37872f7e75c5a6508f96b850a8359
Ahmed hesham esmail <47076435+Ahmedheshamesmail@users.noreply.github.com>
2021-02-13 03:39:51 +0200

    Create nav2.php

---
+++ Include/nav2.php
@@ -0,0 +1,7 @@
+      <nav aria-label="breadcrumb">
+        <ol style="background-color: #f8f9fa;margin-top: 15px;    box-shadow: 1px 1px 3px rgb(0 0 0 / 10%)" class="breadcrumb">
+          <li class="breadcrumb-item"><a href="#">Home</a></li>
+          <li class="breadcrumb-item"><a href="#">Library</a></li>
+          <li class="breadcrumb-item active" aria-current="page">Data</li>
+        </ol>
+      </nav>
