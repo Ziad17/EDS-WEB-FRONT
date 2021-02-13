e5063f71dc54f37837bfb52bd0c0857e26e4d143
ziad17 <ziadmohamd456@gmail.com>
2021-02-13 17:37:43 +0200

    added new file actions

--- Modules/Database/FileFolderAction.php
+++ Modules/Database/FileFolderAction.php
@@ -71,6 +71,8 @@ class FileFolderAction extends Action
             $stmt=$this->getParameterizedStatement($sql,$con,$params);
             if($stmt==false || !sqlsrv_has_rows($stmt) || $this->injectLog($permission_value,$fileID,$con))
             {
+                sqlsrv_rollback($con);
+
                 $this->closeConnection($con);
                 throw new FileNotFoundException("Could not find the data of this file");
             }
@@ -81,18 +83,21 @@ class FileFolderAction extends Action
                 $parent_folder_id=$row[1];
                 $date_created=$row[2];
                 $author_id=$row[3];
-                $current_file_version=$row[4];
-                $version_name=$row[5];
-                $version_notes=$row[6];
-                $version_number=$row[7];
-                $version_date_created=$row[8];
-                $version_author_id=$row[9];
-                $file_type_extension=$row[10];
-                $file_content_id=$row[12];
-                $file_size=$row[13];
+                $file_active=$row[4];
+                $file_locked=$row[5];
+                $file_locked_until=$row[6];
+                $current_file_version=$row[7];
+                $version_name=$row[8];
+                $version_notes=$row[9];
+                $version_number=$row[10];
+                $version_date_created=$row[11];
+                $version_author_id=$row[12];
+                $file_type_extension=$row[13];
+                $file_content_id=$row[14];
+                $file_size=$row[15];
                 $file_version=new FileVersion($current_file_version,$fileID,$version_notes,$version_name,$this->getEmailFromPersonId($version_author_id),
                 $file_content_id,$file_type_extension,$version_number,$version_date_created,$file_size);
-                $file=new File($this->getEmailFromPersonId($author_id),$date_created,$parent_folder_id,$file_version,null,$_fileID);
+                $file=new File($_fileID,$this->getEmailFromPersonId($author_id),$date_created,$parent_folder_id,$file_version,null,$file_locked,$file_locked_until,$file_active);
                 sqlsrv_commit($con);
                 $this->closeConnection($con);
                 return $file;
@@ -110,11 +115,13 @@ class FileFolderAction extends Action
             $permission_value=2**($this->myFilesPermissions->SHOW_VERSIONS_OF_A_FILE);
             $con=$this->getDatabaseConnection();
             sqlsrv_begin_transaction($con);
-            $sql="SELECT * FROM FileWithCurrentVersion_view WHERE ID=?";
+            $sql="SELECT * FROM FileVersions_Type_Content_view WHERE file_id=? ORDER BY date_created ASC";
             $params=array($fileID);
             $stmt=$this->getParameterizedStatement($sql,$con,$params);
             if($stmt==false || !sqlsrv_has_rows($stmt) || $this->injectLog($permission_value,$fileID,$con))
             {
+                sqlsrv_rollback($con);
+
                 $this->closeConnection($con);
                 throw new FileNotFoundException("Could not find the data of this file");
             }
@@ -124,20 +131,24 @@ class FileFolderAction extends Action
                 $arrayOfVersions=array();
                 while($row=sqlsrv_fetch($stmt)) {
                     $row = sqlsrv_fetch_array($stmt)[0];
-                    $version_name = $row[5];
-                    $version_notes = $row[6];
-                    $version_number = $row[7];
-                    $version_date_created = $row[8];
-                    $version_author_id = $row[9];
-                    $file_type_extension = $row[10];
-                    $file_content_id = $row[12];
-                    $file_size = $row[13];
-                    fghfsghfgh
+                    $version_id = $row[0];
+                    $file_id = $row[1];
+                    $version_name = $row[2];
+                    $version_notes = $row[3];
+                    $version_number = $row[4];
+                    $version_date_created = $row[5];
+                    $version_author_id = $row[6];
+                    $file_type_extension = $row[7];
+                    $file_content_id = $row[8];
+                    $file_size = $row[9];
+                    $file_version=new FileVersion($version_id,$fileID,$version_notes,$version_name,$this->getEmailFromPersonId($version_author_id),
+                        $file_content_id,$file_type_extension,$version_number,$version_date_created,$file_size);
+                    $arrayOfVersions[]=$file_version;
                 }
-
+                $file=new File($file->getID(),$file->getAuthorEmail(),$file->getDateCreated(),$file->getParentFolderid(),$file->getCurrentVersion(),$arrayOfVersions,$file->isLocked(),$file->getLockedUntil(),$file->isActive());
                 sqlsrv_commit($con);
                 $this->closeConnection($con);
-                return new File($file->getAuthorEmail(),$file->getDateCreated(),$file->getParentFolderid(),$file->getCurrentVersion(),$arrayOfVersions,$file->getID());
+                return $file;
             }
         } else {
             throw new NoPermissionsGrantedException("User does not have the permissions required for this process");
@@ -150,7 +161,24 @@ class FileFolderAction extends Action
         $this->updateMyPersonFilePermissions($fileID);
         //Required Permission=LOCK_FILE=2
         if ($this->myFilesPermissions->getPermissionsFromBitArray($this->myFilesPermissions->LOCK_FILE)) {
-           //operations
+
+            $permission_value=2**($this->myFilesPermissions->LOCK_FILE);
+            $con=$this->getDatabaseConnection();
+            sqlsrv_begin_transaction($con);
+            //TODO:Test this query to add time
+            $sql="UPDATE File SET locked=true, locked_until=DATEADD(HOUR,2,GETDATE()) WHERE ID=?";
+            $params=array($fileID);
+            $stmt=$this->getParameterizedStatement($sql,$con,$params);
+            if($stmt==false || !sqlsrv_has_rows($stmt) || $this->injectLog($permission_value,$fileID,$con))
+            {
+                sqlsrv_rollback($con);
+                $this->closeConnection($con);
+                throw new FileNotFoundException("Could not find the data of this file");
+            }
+            else
+            {
+                return true;
+            }
         } else {
             throw new NoPermissionsGrantedException("User does not have the permissions required for this process");
         }
@@ -162,19 +190,76 @@ class FileFolderAction extends Action
         $this->updateMyPersonFilePermissions($fileID);
         //Required Permission=UNLOCK_FILE=3
         if ($this->myFilesPermissions->getPermissionsFromBitArray($this->myFilesPermissions->UNLOCK_FILE)) {
-            //operations
+            $this->updateMyPersonFilePermissions($fileID);
+            //Required Permission=LOCK_FILE=2
+            if ($this->myFilesPermissions->getPermissionsFromBitArray($this->myFilesPermissions->UNLOCK_FILE)) {
+
+                $permission_value=2**($this->myFilesPermissions->LOCK_FILE);
+                $con=$this->getDatabaseConnection();
+                sqlsrv_begin_transaction($con);
+                //TODO:Test this query to add time
+                $sql="UPDATE File SET locked=false, locked_until=null WHERE ID=?";
+                $params=array($fileID);
+                $stmt=$this->getParameterizedStatement($sql,$con,$params);
+                if($stmt==false || !sqlsrv_has_rows($stmt) || $this->injectLog($permission_value,$fileID,$con))
+                {
+                    sqlsrv_rollback($con);
+                    $this->closeConnection($con);
+                    throw new FileNotFoundException("Could not find the data of this file");
+                }
+                else
+                {
+                    return true;
+                }
+            } else {
+                throw new NoPermissionsGrantedException("User does not have the permissions required for this process");
+            }
         } else {
             throw new NoPermissionsGrantedException("User does not have the permissions required for this process");
         }
 
     }
-
-    public function modifyFileAddNewVersion(int $fileID,FileVersion $newFIleVersion) : bool
+    
+    public function modifyFileAddNewVersion(int $fileID,FileVersion $newFileVersion) : bool
     {
         $this->updateMyPersonFilePermissions($fileID);
         //Required Permission=$MODIFY_FILE_ADDS_NEW_VERSION=4
         if ($this->myFilesPermissions->getPermissionsFromBitArray($this->myFilesPermissions->MODIFY_FILE_ADDS_NEW_VERSION)) {
-            //operations
+            $permission_value=2**($this->myFilesPermissions->MODIFY_FILE_ADDS_NEW_VERSION);
+            //TODO: Code for storage uploading
+            //FIXME : change the following vars
+            $fileSize=12;//get it from uploading file
+            $fileHref="https://host/storage/file231"; //get it from uploading file
+
+            $con=$this->getDatabaseConnection();
+            sqlsrv_begin_transaction($con);
+            $insertContentSql="INSERT INTO FileContent(href,file_size) VALUES (?,?); SELECT SCOPE_IDENTITY()";
+            $insertContentParams=array($fileHref,$fileSize);
+            $insertContentStmt=$this->getParameterizedStatement($insertContentSql,$con,$insertContentParams);
+            if($insertContentStmt==false || !sqlsrv_has_rows($insertContentStmt))
+            {
+                sqlsrv_rollback($con);
+                $this->closeConnection($con);
+                throw new FileUploadingSqlException("Could not upload the contents of the file");
+            }
+            sqlsrv_next_result($insertContentStmt);
+            sqlsrv_fetch($insertContentStmt);
+            $fileContentID= (int)sqlsrv_get_field($insertContentStmt, 0);
+            $insertVersionSql="INSERT INTO FileVersion(file_id,version_name,version_notes,version_number,date_created,author_id,file_content_id) VALUES (?,?,?,?,GETDATE(),?,?)";
+            $insertVersionParams=array($fileID,$newFileVersion->getName(),$newFileVersion->getNote(),$newFileVersion->getNumber(),$this->getIdFromPersonEmail($newFileVersion->getAuthorEmail()),$fileContentID);
+            $insertVersionStmt=$this->getParameterizedStatement($insertVersionSql,$con,$insertVersionParams);
+            if($insertVersionStmt==false || !sqlsrv_has_rows($insertVersionStmt) || $this->injectLog($permission_value,$fileID,$con))
+            {
+                sqlsrv_rollback($con);
+                $this->closeConnection($con);
+                throw new FileUploadingSqlException("Could not upload the version of the file");
+            }
+            else
+            {
+                sqlsrv_commit($con);
+                $this->closeConnection($con);
+                return true;
+            }
         } else {
             throw new NoPermissionsGrantedException("User does not have the permissions required for this process");
         }
@@ -270,4 +355,7 @@ class FileFolderAction extends Action
     {
         return $this->myFilesPermissions;
     }
+
+
+
 }
--- Modules/File Managment/File.php
+++ Modules/File Managment/File.php
@@ -4,29 +4,40 @@
 class File
 {
     private int $ID;
+
     private string $authorEmail;
     private DateTime $dateCreated;
     private int $parentFolderid;
     private FileVersion $currentVersion;
     private array $fileVersions; //of file versions
 
+    private bool $locked;
+    private DateTime $lockedUntil;
+    private bool $active;
+
     /**
-     * Folder constructor.
-     * @param String $authorEmail
+     * File constructor.
+     * @param int $ID
+     * @param string $authorEmail
      * @param DateTime $dateCreated
-     * @param int $parentFolder
+     * @param int $parentFolderid
      * @param FileVersion $currentVersion
      * @param array $fileVersions
-     * @param int $ID
+     * @param bool $locked
+     * @param DateTime $lockedUntil
+     * @param bool $active
      */
-    public function __construct(string $authorEmail, DateTime $dateCreated, int $parentFolder, FileVersion $currentVersion, array $fileVersions, int $ID)
+    public function __construct(int $ID, string $authorEmail, DateTime $dateCreated, int $parentFolderid, FileVersion $currentVersion, array $fileVersions, bool $locked, DateTime $lockedUntil, bool $active)
     {
+        $this->ID = $ID;
         $this->authorEmail = $authorEmail;
         $this->dateCreated = $dateCreated;
-        $this->parentFolderid = $parentFolder;
+        $this->parentFolderid = $parentFolderid;
         $this->currentVersion = $currentVersion;
         $this->fileVersions = $fileVersions;
-        $this->ID = $ID;
+        $this->locked = $locked;
+        $this->lockedUntil = $lockedUntil;
+        $this->active = $active;
     }
 
 
@@ -79,6 +90,30 @@ class File
     public function getID(): int
     {
         return $this->ID;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLocked(): bool
+    {
+        return $this->locked;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getLockedUntil(): DateTime
+    {
+        return $this->lockedUntil;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isActive(): bool
+    {
+        return $this->active;
     }
 }
 
