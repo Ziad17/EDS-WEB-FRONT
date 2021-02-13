27b7b499cafd9e4fb2e9f2223b2ff50ae7289d41
Ahmed hesham esmail <47076435+Ahmedheshamesmail@users.noreply.github.com>
2021-02-13 07:48:40 +0200

    Update nav2.php

--- Include/nav2.php
+++ Include/nav2.php
@@ -1,7 +1,15 @@
       <nav aria-label="breadcrumb">
         <ol style="background-color: #f8f9fa;margin-top: 15px;    box-shadow: 1px 1px 3px rgb(0 0 0 / 10%)" class="breadcrumb">
-          <li class="breadcrumb-item"><a href="#">Home</a></li>
-          <li class="breadcrumb-item"><a href="#">Library</a></li>
-          <li class="breadcrumb-item active" aria-current="page">Data</li>
+          <li class="breadcrumb-item"><a href="Home.php">KfsDocs</a></li>
+          <?php
+           if (isset($name)) {
+           	if ($name == 'KfsDocs') {
+           		echo  '<li class="breadcrumb-item">....</li>';
+           	}else{
+           	echo  '<li class="breadcrumb-item"><a href="'.$name.'.php">'.$name.'</a></li>';
+           	 ;
+           } 
+       }
+           ?>
         </ol>
       </nav>
