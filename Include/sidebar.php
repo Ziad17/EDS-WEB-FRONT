533dcd52ccc8d86191d3c5ef2765aa8a1a6d60e9
Ahmed hesham esmail <47076435+Ahmedheshamesmail@users.noreply.github.com>
2021-02-13 07:49:34 +0200

    Update sidebar.php

--- Include/sidebar.php
+++ Include/sidebar.php
@@ -1,43 +1,34 @@
 			<nav  id="sidebar">
 				<div class="p-4  sidch">
-		  		<a href="img/undraw_male_avatar_323b.svg"><img style="border-radius: 50%;padding: 5px" src="img/undraw_male_avatar_323b.svg" width="100px" height="100px" alt=""></a>
-					<h6  style="color: #fff;text-shadow: 5px 5px 5px #fff">Eng:Ahmed Hesham</h6>
-					<span class="country">Web Designer</span>
-					<hr style="color: #fff;background-color: #fff;">
+		  		<div align="center">
+            <img style="border-radius: 50%;padding: 5px" src="img/undraw_male_avatar_323b.svg" width="100px" height="100px" alt="Profile_Img">
+            <h6  style="color: #fff;text-shadow: 5px 5px 5px #fff">Eng:Ahmed Hesham</h6>
+            <span class="country">Web Designer</span>
+            <hr style="color: #fff;background-color: #fff;font-size: 15px;  border-style: inset;
+            border-width: 1px;">
+          </div>
 	        <ul  class="list-unstyled components mb-5">
-	          <li class="active">
-	            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-home"></i> Home &nbsp &nbsp </a>
-	            <ul class="collapse list-unstyled" id="homeSubmenu">
-                <li>
-                    <a class="drobli" href="#">Home 1</a>
-                </li>
-                <li>
-                    <a class="drobli" href="#">Home 2</a>
-                </li>
-                <li>
-                    <a class="drobli" href="#">Home 3</a>
-                </li>
-	            </ul>
-	          </li>
-	          <li>
+	          <li <?php if ($name === 'KfsDocs') {echo 'class="active"';};?>>
+              <a href="Home.php"><i class="fas fa-home"></i> Home</a>
+	          <li  <?php if ($name === 'Project') {echo 'class="active"';};?>>
 	              <a href="#"><i class="fas fa-project-diagram"></i> Project</a>
 	          </li>
 	          <li>
-              <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"> <i class="fas fa-university"></i> Colleges </a>
+              <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"> <i class="far fa-folder-open"></i> Faculty Folder </a>
               <ul class="collapse list-unstyled" id="pageSubmenu">
-                <li>
-                    <a class="drobli" href="#"><i class="fas fa-vials"></i> Science and Engineering</a>
+                <li >
+                    <a class="drobli" href="#"><i class="fas fa-laptop-code"></i> Computer and Info</a>
                 </li>
                 <li>
-                    <a class="drobli" href="#"><i class="fas fa-hand-holding-medical"></i> Medical colleges</a>
+                    <a class="drobli" href="#"><i class="fas fa-book-reader"></i>  Arts</a>
                 </li>
                 <li>
-                    <a class="drobli" href="#"><i class="far fa-building"></i> Humanitarian and educational</a>
+                    <a class="drobli" href="#"><i class="fas fa-cash-register"></i> Commerce</a>
                 </li>
               </ul>
 	          </li>
-	          <li>
-              <a href="#">Portfolio</a>
+	          <li <?php if ($name === 'Profile') {echo 'class="active"';};?>>
+              <a href="profile.php"><i class="far fa-user"></i> Profile</a>
 	          </li>
 	          <li>
               <a  href="#">
