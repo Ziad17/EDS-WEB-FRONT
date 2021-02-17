57f97bb9832b8aaf8e2aa475802fdcedabe2be05
Ahmed hesham esmail <47076435+Ahmedheshamesmail@users.noreply.github.com>
2021-02-17 11:23:31 +0200

    Update nav1.php

--- Include/nav1.php
+++ Include/nav1.php
@@ -1,16 +1,20 @@
+
 <div>
+            <?php 
+            if ( $name == 'Activity') {
+echo ' 
       <ul class="nav nav-tabs" id="myTab" role="tablist">
         <li class="nav-item" role="presentation">
           <!-- Button Create new section modal -->
           <button type="button" class="btn " data-toggle="modal" data-target="#section">
-          <i class="fas fa-stream"></i> Create new section
+          <i class="fas fa-file-medical"></i> Add new File
           </button> 
             <!-- Modal Create new section -->
           <div class="modal fade" id="section" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
               <div class="modal-content ">
                 <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-stream"></i> New Section</h5>
+                  <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-file-medical"></i> Add File</h5>
                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                   </button>
@@ -32,17 +36,18 @@
           </div>
           <!--End Modal section -->
         </li>
+
           <li class="nav-item" role="presentation">
           <!--   Button Create new Folder modal -->
         <button type="button" class="btn " data-toggle="modal" data-target="#Folder">
-          <i class="fas fa-folder-plus"></i> Create new Folder
+          <i class="fas fa-folder-plus"></i> Add new Folder
         </button> 
           <!-- Modal Create new Folder -->
         <div class="modal fade" id="Folder" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog">
             <div class="modal-content ">
               <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-folder-plus"></i> New Folder</h5>
+                <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-folder-plus"></i> Add Folder</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
@@ -65,17 +70,8 @@
         <!--End ModalCreate new Folder -->
           </li>
 
-<!--           <li class="nav-item" role="presentation">
-            <div class="dropdown">
-              <button class="btn  dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="fas fa-chevron-circle-down"></i> Dropdown button
-              </button>
-              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </div> 
-          </li> -->
+         ';
+            }
+          ?>
         </ul>
       </div>
