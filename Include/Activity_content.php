0d24e95bcb1374a29b84ee0c9a0ce787c132f2a9
Ahmed hesham esmail <47076435+Ahmedheshamesmail@users.noreply.github.com>
2021-02-17 11:23:27 +0200

    Create Activity_content.php

---
+++ Include/Activity_content.php
@@ -0,0 +1,238 @@
+<ul class="nav nav-tabs m-auto" id="myTab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Public Activity</a>
+  </li>
+  <li class="nav-item" role="presentation">
+    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">My Mention</a>
+  </li>
+
+
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+    
+    <table id="table" border='2' class="table table-hover table-responsive-md mt-3">
+  <thead>
+    <tr>
+      <th scope="col">NO</th>
+      <th scope="col">File_Name</th>
+      <th scope="col">File_Type</th>
+      <th scope="col">File_Size</th>
+      <th scope="col">Data_Created</th>
+      <th scope="col">Author_Name</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>1.pdf</td>
+      <td>Pdf</td>
+      <td>1kb</td>
+      <td>12/8/2012</td>
+      <td>Ahmed</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Security.docx</td>
+      <td>Word</td>
+      <td>16kb</td>
+      <td>24/9/2020</td>
+      <td>Yousef</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>1.pdf</td>
+      <td>Pdf</td>
+      <td>1kb</td>
+      <td>12/8/2012</td>
+      <td>Ahmed</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>1.pdf</td>
+      <td>Pdf</td>
+      <td>1kb</td>
+      <td>12/8/2012</td>
+      <td>Ahmed</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Security.docx</td>
+      <td>Word</td>
+      <td>16kb</td>
+      <td>24/9/2020</td>
+      <td>Yousef</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>1.pdf</td>
+      <td>Pdf</td>
+      <td>1kb</td>
+      <td>12/8/2012</td>
+      <td>Ahmed</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>1.pdf</td>
+      <td>Pdf</td>
+      <td>1kb</td>
+      <td>12/8/2012</td>
+      <td>Ahmed</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Security.docx</td>
+      <td>Word</td>
+      <td>16kb</td>
+      <td>24/9/2020</td>
+      <td>Yousef</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>1.pdf</td>
+      <td>Pdf</td>
+      <td>1kb</td>
+      <td>12/8/2012</td>
+      <td>Ahmed</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>1.pdf</td>
+      <td>Pdf</td>
+      <td>1kb</td>
+      <td>12/8/2012</td>
+      <td>Ahmed</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Security.docx</td>
+      <td>Word</td>
+      <td>16kb</td>
+      <td>24/9/2020</td>
+      <td>Yousef</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>1.pdf</td>
+      <td>Pdf</td>
+      <td>1kb</td>
+      <td>12/8/2012</td>
+      <td>Ahmed</td>
+    </tr>
+  </tbody>
+</table>
+
+  </div>
+  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+    <table id="table2" border='2' class="table table-hover table-responsive-md mt-3">
+  <thead>
+    <tr>
+      <th scope="col">NO</th>
+      <th scope="col">File_Name</th>
+      <th scope="col">File_Type</th>
+      <th scope="col">File_Size</th>
+      <th scope="col">Data_Created</th>
+      <th scope="col">Author_Name</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>1.pdf</td>
+      <td>Pdf</td>
+      <td>1kb</td>
+      <td>12/8/2012</td>
+      <td>Ahmed</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Security.docx</td>
+      <td>Word</td>
+      <td>16kb</td>
+      <td>24/9/2020</td>
+      <td>Yousef</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>1.pdf</td>
+      <td>Pdf</td>
+      <td>1kb</td>
+      <td>12/8/2012</td>
+      <td>Ahmed</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>1.pdf</td>
+      <td>Pdf</td>
+      <td>1kb</td>
+      <td>12/8/2012</td>
+      <td>Ahmed</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Security.docx</td>
+      <td>Word</td>
+      <td>16kb</td>
+      <td>24/9/2020</td>
+      <td>Yousef</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>1.pdf</td>
+      <td>Pdf</td>
+      <td>1kb</td>
+      <td>12/8/2012</td>
+      <td>Ahmed</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>1.pdf</td>
+      <td>Pdf</td>
+      <td>1kb</td>
+      <td>12/8/2012</td>
+      <td>Ahmed</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Security.docx</td>
+      <td>Word</td>
+      <td>16kb</td>
+      <td>24/9/2020</td>
+      <td>Yousef</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>1.pdf</td>
+      <td>Pdf</td>
+      <td>1kb</td>
+      <td>12/8/2012</td>
+      <td>Ahmed</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>1.pdf</td>
+      <td>Pdf</td>
+      <td>1kb</td>
+      <td>12/8/2012</td>
+      <td>Ahmed</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Security.docx</td>
+      <td>Word</td>
+      <td>16kb</td>
+      <td>24/9/2020</td>
+      <td>Yousef</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>1.pdf</td>
+      <td>Pdf</td>
+      <td>1kb</td>
+      <td>12/8/2012</td>
+      <td>Ahmed</td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+</div>
