5bde50d96f405ed8a4efcb22095d58ebd885f04a
Ahmed hesham esmail <47076435+Ahmedheshamesmail@users.noreply.github.com>
2021-03-28 17:58:46 +0200

    Update nav2.php

--- Include/nav2.php
+++ Include/nav2.php
@@ -1,4 +1,4 @@
-      <nav aria-label="breadcrumb">
+<!--       <nav aria-label="breadcrumb">
         <ol style="background-color: #f8f9fa;margin-top: 15px;    box-shadow: 1px 1px 3px rgb(0 0 0 / 10%)" class="breadcrumb">
           <li class="breadcrumb-item"><a href="Home.php">KfsDocs</a></li>
           <?php
@@ -12,4 +12,4 @@
        }
            ?>
         </ol>
-      </nav>
+      </nav> -->
