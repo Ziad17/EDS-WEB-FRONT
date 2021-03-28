230357b249bb78f1567eb6ee553afb014af8a687
Ahmed hesham esmail <47076435+Ahmedheshamesmail@users.noreply.github.com>
2021-03-28 17:58:51 +0200

    Update nav3.php

--- Include/nav3.php
+++ Include/nav3.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav align="center" style="border: 1px solid ;border-radius: 20px" class="navbar navbar-expand-lg navbar-light bg-light">
           <div class="container-fluid">
             <button  type="button" id="sidebarCollapse" class="btn btn-outline-dark">
               <i  class="fas fa-chevron-right"></i>
@@ -9,27 +9,48 @@
             aria-label="Toggle navigation">
                 <i class="fa fa-bars"></i>
             </button>
-            <div align="center" class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="nav navbar-nav ml-auto ">
-                <li class="nav-item">
-                    <a class="nav-link <?php if ($name === 'KfsDocs') {echo ' active"';} ?>" href="Home.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?php if ($name === 'About') {echo ' active"';} ?>" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?php if ($name === 'Profile') {echo ' active"';} ?>" href="profile.php">Profile</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?php if ($name === 'Activity') {echo ' active"';} ?>" href="Activity.php">Activity</a>
-                </li>
-                  <li class="nav-item">
-                      <form method="post">
+             <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ol style="margin-left: 20px" class="navbar-nav mr-auto">
+                  <li class="breadcrumb-item"><a href="Home.php">KfsDocs</a></li>
+                    <?php
+                       if (isset($name)) {
+                          if ($name == 'KfsDocs') {
+                            echo  '<li class="breadcrumb-item">....</li>';
+                          }else{
+                          echo  '<li class="breadcrumb-item"><a href="'.$name.'.php">'.$name.'</a></li>';
+                           ;
+                         } 
+                       }
+                   ?>
+                </ol>
+                 <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <!-- <button class="btn my-2 my-sm-0" type="submit"><i style="font-size: 20px" class="fas fa-search"></i></button> -->
+    </form>
+    <button class="btn my-2 my-sm-0" type="submit"><i style="font-size: 20px" class="fas fa-bookmark"></i></button><br>
 
-                      <input class="nav-link btn btn-danger" type="submit" name="logout" value="logout"></input>
-                      </form>
-                  </li>
-              </ul>
-            </div>
+      <div  class="dropdown mr-1">
+    <button style="margin: auto;" type="button" class="btn btn-sm dropdown-toggle" id="dropdownNotif" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,20">
+      <i style="font-size: 20px" class="fas fa-bell"></i>
+    </button>
+    <div  class="dropdown-menu" aria-labelledby="dropdownNotif">
+      <a class="dropdown-item" href="#">Notification 1</a>
+      <a class="dropdown-item" href="#">Notification 2</a>
+      <a class="dropdown-item" href="#">Notification 3</a>
+      <a class="dropdown-item" href="#">Notification 4</a>
+    </div>
+  </div>
+
+  <div class="dropdown mr-1">
+    <button style="margin: auto;" type="button" class="btn btn-sm dropdown-toggle" id="dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,20">
+      <i style="font-size: 20px" class="fas fa-ellipsis-v"></i>
+    </button>
+    <div align="center"  class="dropdown-menu" aria-labelledby="dropdownMenu">
+      <a style="background-color: #f8b739;text-align: center; " class="dropdown-item" href="#">Logout <i class="fas fa-sign-out-alt"></i></a>
+    </div>
+  </div>
+
+</div>
+          
           </div>
         </nav>
