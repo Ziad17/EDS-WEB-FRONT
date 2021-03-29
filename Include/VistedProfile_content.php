e06bca3e2583d23092d7931d5dece6301979e84c
Ahmed hesham esmail <47076435+Ahmedheshamesmail@users.noreply.github.com>
2021-03-30 01:42:37 +0200

    Create VistedProfile_content.php

---
+++ Include/VistedProfile_content.php
@@ -0,0 +1,132 @@
+<div  class="col-md-4">
+	<div class="card-group">
+		<div class="card" style="width: 18rem;padding: 20px;border-top: 3px  solid #f8b739;border-bottom: 3px  solid #f8b739 ">
+			<img style="border-radius: 50%;padding: 5px" src="img/undraw_female_avatar_w3jk (1).svg" width="100px" height="100px" class="card-img-top" alt="...">
+			<div class="card-body">
+				<h5 class="card-title">User Name</h5>
+				
+					<p class="card-text"> JobRoleName</p>
+					<p class="card-text"> instituionName</p>
+					<p class="card-text"> HiredDate</p>
+				</pre>
+				<br>
+				<br>
+
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="col-md-8">
+	<ul class="nav nav-tabs m-auto" id="myTab" role="tablist">
+  <li class="nav-item" role="presentation">
+    <a class="nav-link active" id="Public-tab" data-toggle="tab" href="#Public" role="tab" aria-controls="Public" aria-selected="true">Public Activity</a>
+  </li>
+
+  <li class="nav-item" role="presentation">
+    <a class="nav-link" id="Inolved-tab" data-toggle="tab" href="#Inolved" role="tab" aria-controls="Inolved" aria-selected="false">Inolved With</a>
+  </li>
+
+    <li class="nav-item" role="presentation">
+    <a class="nav-link" id="Related-tab" data-toggle="tab" href="#Related" role="tab" aria-controls="Related" aria-selected="false">Related With You</a>
+  </li>
+
+
+</ul>
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="Public" role="tabpanel" aria-labelledby="Public-tab">
+    
+    <table id="table" border='2' class="table table-responsive-md mt-3">
+  		<thead>
+	    	<tr class="bg-dark text-light">
+		      <th scope="col">File_Name</th>
+		      <th scope="col">File_Type</th>
+		      <th scope="col">File_Size</th>
+		      <th scope="col">Data_Created</th>
+		      <th scope="col">Author_Name</th>
+	    	</tr>
+  		</thead>
+		  <tbody>
+		    <tr style="height: 40px">
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    </tr>
+		    <tr style="height: 40px"> 
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    </tr>
+		  </tbody>
+</table>
+
+  </div>
+  <div class="tab-pane fade" id="Inolved" role="tabpanel" aria-labelledby="Inolved-tab">
+      <table id="table2" border='2' class="table table-responsive-md mt-3">
+  		<thead>
+	    	<tr class="bg-dark text-light">
+		      <th scope="col">File_Name</th>
+		      <th scope="col">File_Type</th>
+		      <th scope="col">File_Size</th>
+		      <th scope="col">Data_Created</th>
+		      <th scope="col">Author_Name</th>
+	    	</tr>
+  		</thead>
+		  <tbody>
+		    <tr style="height: 40px">
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    </tr>
+		    <tr style="height: 40px"> 
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    </tr>
+		  </tbody>
+</table>
+  </div>
+  <div class="tab-pane fade " id="Related" role="tabpanel" aria-labelledby="Related-tab">
+  	    <table id="table3" border='2' class="table table-responsive-md mt-3">
+  		<thead>
+	    	<tr class="bg-dark text-light">
+		      <th scope="col">File_Name</th>
+		      <th scope="col">File_Type</th>
+		      <th scope="col">File_Size</th>
+		      <th scope="col">Data_Created</th>
+		      <th scope="col">Author_Name</th>
+	    	</tr>
+  		</thead>
+		  <tbody>
+		    <tr style="height: 40px">
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    </tr>
+		    <tr style="height: 40px"> 
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    	<td></td>
+		    </tr>
+		  </tbody>
+</table>
+  </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+</div>
