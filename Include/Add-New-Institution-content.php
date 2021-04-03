159ce72b3d3ff6aa1cc425d6389b3f78a8a8b484
Ahmed hesham esmail <47076435+Ahmedheshamesmail@users.noreply.github.com>
2021-04-04 00:29:17 +0200

    Create Add-New-Institution-content.php

---
+++ Include/Add-New-Institution-content.php
@@ -0,0 +1,58 @@
+<div class="col-md-12">
+  <form action="" method="POST">
+    <div class="row">
+      <div class="col-12">
+        <input type="text" class="form-control mb-3" name="name" placeholder="Name" value="" required>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <input type="text" class="form-control mb-3" name="Phone-Number" placeholder="Phone Number(Optional)" value="" >
+      </div>
+      <div class="col-md-6">
+        <input type="text" class="form-control mb-3" name="Sec-Num" placeholder="Secondary Number(Optional)" value="" >
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        
+        <input type="text" class="form-control mb-3" name="Fax" placeholder="Fax(Optional)" value="" >
+      </div>
+      <div class="col-md-6">
+        
+        <input type="Email" class="form-control mb-3" name="Email" placeholder="Email or Website (Optional)" value="" required>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 mb-5">
+        <h6>Supervisor Institution</h6>
+        <select class="form-control" required>
+          <option value="">University Managment</option>
+          <option>...</option>
+        </select>
+      </div>
+      <div class="col-md-6 mb-5">
+        <h6>Institution Type</h6>
+        <select class="form-control" required>
+          <option value="">General Manager</option>
+          <option>...</option>
+        </select>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3">
+        
+      </div>
+      <div class="col-md-3 mb-5">
+         <button class="w-100 btn btn-warning btn-lg text-light" type="submit">Reset</button>
+       </div>
+       <div class="col-md-3 mb-5">
+         <button class="w-100 btn btn-warning btn-lg text-light" type="submit">Confirm</button>
+       </div>
+        <div class="col-md-3 mb-5">
+      
+      </div> 
+    </div>
+  </form>
+  <br>
+</div>
