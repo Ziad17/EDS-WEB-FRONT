2c47152ff6b725c178a2fbd83048588690ab7916
Ahmed hesham esmail <47076435+Ahmedheshamesmail@users.noreply.github.com>
2021-04-04 00:29:13 +0200

    Create Add-New-Position-Content.php

---
+++ Include/Add-New-Position-Content.php
@@ -0,0 +1,112 @@
+<div class="col-md-12">
+  <form action="" method="POST">
+    <div class="row">
+      <div class="col-md-7">
+        <h6>.</h6>
+        <input type="text" class="form-control mb-3" name="Position-Name" placeholder="Position Name" value="" required>
+      </div>
+      <div class="col-md-5">
+        <h6>Priority level</h6>
+        <select class="form-control" required>
+          <option value="">Senior A</option>
+          <option>...</option>
+        </select>
+      </div>
+    </div>
+    <div class="row mb-5">
+      <div class="col-md-6">
+        <h5>File Permissions</h5>
+        <table border="3" style="padding: 5px;width: 90%">
+          <tbody>
+            <tr>
+              <td style="padding: 15px"><input type="checkbox" name=""></td>
+              <td style="padding: 20px">Read Level A File</td>
+            </tr>
+            <tr>
+              <td style="padding: 15px"><input type="checkbox" name=""></td>
+              <td style="padding: 20px">Read Level B File</td>
+            </tr>
+            <tr>
+              <td style="padding: 15px"><input type="checkbox" name=""></td>
+              <td style="padding: 20px">Read Level C File</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="col-md-6">
+        <h5>Folder Permissions</h5>
+        <table border="3" style="padding: 5px;width: 90%">
+          <tbody>
+            <tr>
+              <td style="padding: 15px"><input type="checkbox" name=""></td>
+              <td style="padding: 20px">Creat File in Folder</td>
+            </tr>
+            <tr>
+              <td style="padding: 15px"><input type="checkbox" name=""></td>
+              <td style="padding: 20px">Read File of A Folder</td>
+            </tr>
+            <tr>
+              <td style="padding: 15px"><input type="checkbox" name=""></td>
+              <td style="padding: 20px"></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="row mb-5">
+      <div class="col-md-6">
+        <h5>Person Permissions</h5>
+        <table border="3" style="padding: 5px;width: 90%">
+          <tbody>
+            <tr>
+              <td style="padding: 15px"><input type="checkbox" name=""></td>
+              <td style="padding: 20px">Add Person</td>
+            </tr>
+            <tr>
+              <td style="padding: 15px"><input type="checkbox" name=""></td>
+              <td style="padding: 20px">Assign Role To Person</td>
+            </tr>
+            <tr>
+              <td style="padding: 15px"><input type="checkbox" name=""></td>
+              <td style="padding: 20px">Deactivate Person</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="col-md-6">
+        <h5>Institution Permissions</h5>
+        <table border="3" style="padding: 5px;width: 90%">
+          <tbody>
+            <tr>
+              <td style="padding: 15px"><input type="checkbox" name=""></td>
+              <td style="padding: 20px">Create Institution</td>
+            </tr>
+            <tr>
+              <td style="padding: 15px"><input type="checkbox" name=""></td>
+              <td style="padding: 20px">View Institution Information</td>
+            </tr>
+            <tr>
+              <td style="padding: 15px"><input type="checkbox" name=""></td>
+              <td style="padding: 20px">Edite Institution</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3">
+        
+      </div>
+      <div class="col-md-3 mb-5">
+         <button class="w-100 btn btn-warning btn-lg text-light" type="submit">Reset</button>
+       </div>
+       <div class="col-md-3 mb-5">
+         <button class="w-100 btn btn-warning btn-lg text-light" type="submit">Confirm</button>
+       </div>
+        <div class="col-md-3 mb-5">
+      
+      </div> 
+    </div>
+  </form>
+  <br>
+</div>
