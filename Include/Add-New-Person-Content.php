5f850a5193219240095761205c3a7689b28d6407
Ahmed hesham esmail <47076435+Ahmedheshamesmail@users.noreply.github.com>
2021-04-04 00:29:00 +0200

    Create Add-New-Person-Content.php

---
+++ Include/Add-New-Person-Content.php
@@ -0,0 +1,84 @@
+<div class="col-md-12">
+  <form action="" method="POST">
+    <div class="row">
+      <div class="col-12">
+        <input type="text" class="form-control mb-3" name="name" placeholder="Name" value="" required>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <input type="text" class="form-control mb-3" name="Academic-Number" placeholder="Academic Number" value="" required>
+      </div>
+      <div class="col-md-6">
+        <input type="email" class="form-control mb-3" name="email" placeholder="Email" value="" required>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        
+        <input type="password" class="form-control mb-3" name="password" placeholder="Password" value="" required>
+      </div>
+      <div class="col-md-6">
+        
+        <input type="password" class="form-control mb-3" name="Confirm-password" placeholder="Confirm Password" value="" required>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <label>.</label>
+        <input type="text" class="form-control mb-3" name="Job-Title" placeholder="Job Title" value="" required>
+      </div>
+      <div class="col-md-6">
+        <label>Date-Hired</label>
+        <input type="date" class="form-control mb-3" name="Date-Hired" placeholder="Date Hired" value="" required>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 mb-3">
+        <h6>Institution  <a href="Add-New-Institution.php"><u><b> Add New Institution ?</b></u></a></h6>
+        <select class="form-control" required>
+          <option value="">University Managment</option>
+          <option>...</option>
+        </select>
+      </div>
+      <div class="col-md-6 mb-3">
+        <h6>Position  <a href="Add-New-Position.php"><u><b> Add New Position ?</b></u></a></h6>
+        <select class="form-control" required>
+          <option value="">General Manager</option>
+          <option>...</option>
+        </select>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 mb-5">
+        <h6>Gender</h6>
+        <select class="form-control" required>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </select>
+      </div>
+      <div class="col-md-6 mb-5">
+        <h6>City</h6>
+        <select class="form-control" required>
+          <option value="">Alexandria</option>
+          <option>...</option>
+        </select>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3">
+        
+      </div>
+      <div class="col-md-3 mb-5">
+         <button class="w-100 btn btn-warning btn-lg text-light" type="submit">Reset</button>
+       </div>
+       <div class="col-md-3 mb-5">
+         <button class="w-100 btn btn-warning btn-lg text-light" type="submit">Confirm</button>
+       </div>
+        <div class="col-md-3 mb-5">
+      
+      </div> 
+    </div>
+  </form>
+  <br>
+</div>
