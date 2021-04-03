802e3164d553db3bd4e644f28b5c5341ec357a7d
Ahmed hesham esmail <47076435+Ahmedheshamesmail@users.noreply.github.com>
2021-04-04 00:27:47 +0200

    Update VistedProfile_content.php

--- Include/VistedProfile_content.php
+++ Include/VistedProfile_content.php
@@ -1,6 +1,6 @@
-<div  class="col-md-4">
+<div class="col-md-3">
 	<div class="card-group">
-		<div class="card" style="width: 18rem;padding: 20px;border-top: 3px  solid #f8b739;border-bottom: 3px  solid #f8b739 ">
+		<div class="card" style="padding: 20px;border-top: 3px  solid #f8b739;border-bottom: 3px  solid #f8b739 ">
 			<img style="border-radius: 50%;padding: 5px" src="img/undraw_female_avatar_w3jk (1).svg" width="100px" height="100px" class="card-img-top" alt="...">
 			<div class="card-body">
 				<h5 class="card-title">User Name</h5>
@@ -16,10 +16,25 @@
 		</div>
 	</div>
 </div>
-
-
-<div class="col-md-8">
-	<ul class="nav nav-tabs m-auto" id="myTab" role="tablist">
+<!----------------------------------------------------------------------->
+<!----------------------------------------------------------------------->
+<div class="col-md-9">
+	<div class="row">
+		<div class="col-md-7">
+			
+		</div>
+		<div class="col-md-5">
+			<nav style="border:1px solid;padding: 10px" class="navbar navbar-light bg-light  mx-auto d-block  border-dark">
+			  <div align="right">
+			   	<button class="btn my-2 my-sm-0" type="submit"><i style="font-size: 30px" class="fas fa-lock"></i></button>
+			   	<button class="btn my-2 my-sm-0" type="submit"><i style="font-size: 30px" class="fab fa-telegram-plane"></i></button>
+			  </div>
+			</nav>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<ul class="nav nav-tabs m-auto" id="myTab" role="tablist">
   <li class="nav-item" role="presentation">
     <a class="nav-link active" id="Public-tab" data-toggle="tab" href="#Public" role="tab" aria-controls="Public" aria-selected="true">Public Activity</a>
   </li>
@@ -129,4 +144,7 @@
 <br>
 <br>
 <br>
+		</div>
+
+	</div>
 </div>
